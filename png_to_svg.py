60ddd9fb9caa5868cee668340bc7ee1f94fac007
CHAILLOU Paul <paul.chaillou@inria.fr>
2022-03-14 11:58:08 +0100

    ajout du script python png_to_svg.py

---
+++ png_to_svg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 16:31:46 2022
+
+@author: pchaillo
+
+"""
+
+#import nest_asyncio
+#nest_asyncio.apply()
+#__import__('IPython').embed()
+
+from pathlib import Path
+from svgtrace import trace
+
+# récupère la localisation des fichiers
+THISDIR = str(Path(__file__).resolve().parent)
+
+# converti le png en svg (bitmap trace equivalent)
+bw = open(THISDIR + "/loutre_1p_new.svg", "w")
+bw.write(trace(THISDIR + "/loutre_1p.png", True))
+bw.close()
+colour = open(THISDIR + "/loutre_1p_new.svg", "w")
+colour.write(trace(THISDIR + "/loutre_1p.png"))
+colour.close()
+
